4ee75980b68ba7de37e871119ab4a239d8700cc8
Serhii Klunniy <s.klunniy@gmail.com>
2023-07-03 17:54:30 +0300

    commit

---
+++ src/main/java/org/example/model/Type.java
@@ -0,0 +1,19 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Serhii Klunniy
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Type {
+
+    private int id;
+    private String name;
+    private String string;
+
+}
---
+++ src/main/test/org/example/service/AnimalSerializerTest.java
@@ -0,0 +1,37 @@
+package org.example.service;
+
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.example.model.Animal;
+import org.example.service.AnimalSerializer;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Serhii Klunniy
+ */
+public class AnimalSerializerTest {
+
+    private final AnimalSerializer animalSerializer = new AnimalSerializer(new YAMLMapper());
+    private final Animal dog = new Animal("sharik", "2015.03.11", 7, false, "doberman");
+    private final Animal cat = new Animal("myrka", "2016.04.20", 7, false, "persian");
+
+
+    @Test
+    public void serializeTest() {
+        animalSerializer.serialize(dog);
+    }
+
+    @Test
+    public void serializeListTest() {
+        animalSerializer.serialize(List.of(cat, dog));
+    }
+
+    @Test
+    public void deserializeTest() {
+        Optional<Animal> deserialize = animalSerializer.deserialize();
+        Assert.assertEquals(dog, deserialize.orElse(null));
+    }
+}
